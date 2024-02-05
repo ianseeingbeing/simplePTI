9e0567d882132dece6ed9d9f076f4624dfd9bc21
ianseeingbeing <igillette2006@gmail.com>
2024-02-05 11:59:11 -0500

    initialized repo

---
+++ build/CMakeFiles/PTI.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PTI.dir/main.cpp.o"
+  "CMakeFiles/PTI.dir/main.cpp.o.d"
+  "PTI"
+  "PTI.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PTI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/pti.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pti.dir/main.cpp.o"
+  "CMakeFiles/pti.dir/main.cpp.o.d"
+  "pti"
+  "pti.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pti.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
